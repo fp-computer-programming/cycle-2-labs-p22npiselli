d8d4e420301d4c84151bc9ad20a34921e0c08849
p22npiselli <p22npiselli@fairfieldprep.org>
2021-09-21 10:05:49 -0400

    lab 6-1

---
+++ lab_6-1.py
@@ -0,0 +1,10 @@
+# Author: Nolan (AMDG) 9/21/2021
+
+first_name = input("Enter your first name ")
+last_name = input("Enter your last name ")
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+
